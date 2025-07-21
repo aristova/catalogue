7b4c7cc73f96803b0ca2760328bf6a71baafc477
emerald-sea <alrai.cepheus@gmail.com>
2025-07-21 11:55:08 +0100

    Change messages

--- src/main/java/com/example/demo/service/CustomUserDetailsService.java
+++ src/main/java/com/example/demo/service/CustomUserDetailsService.java
@@ -1,7 +1,9 @@
 package com.example.demo.service;
 
 import com.example.demo.entity.User;
+import com.example.demo.exception.NotFoundException;
 import com.example.demo.repository.UserRepository;
+import com.example.demo.util.MessageLocalizer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.*;
 import org.springframework.stereotype.Service;
@@ -12,12 +14,13 @@ import java.util.Collections;
 @RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
     private final UserRepository userRepository;
+    private final MessageLocalizer localizer;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByUsername(username);
         if (user == null) {
-            throw new UsernameNotFoundException("User Not Found with username: " + username);
+            throw new NotFoundException(localizer.getMessage("user.not.found", username));
         }
         return new org.springframework.security.core.userdetails.User(
                 user.getUsername(),
